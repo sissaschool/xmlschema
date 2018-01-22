eb3dd81e659ebce1fc96ee4d6ef514f0684ce12b
Davide Brunato <brunato@ubik.localdomain>
2018-01-22 17:58:46 +0100

    Add pull request #36 and set version to 0.9.20

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.rst") as readme:
 
 setup(
     name='xmlschema',
-    version='0.9.19',
+    version='0.9.20',
     packages=['xmlschema', 'xmlschema.validators', 'xmlschema.tests'],
     package_data={'xmlschema': [
         'unicode_categories.json',
