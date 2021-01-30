f254ac500bff0e7d811c6711cae69f70ae222b79
Davide Brunato <brunato@sissa.it>
2021-01-30 18:37:01 +0100

    Refactor converters.py module to a subpackage

---
+++ xmlschema/converters/__init__.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c), 2016-2021, SISSA (International School for Advanced Studies).
+# All rights reserved.
+# This file is distributed under the terms of the MIT License.
+# See the file 'LICENSE' in the root directory of the present
+# distribution, or http://opensource.org/licenses/MIT.
+#
+# @author Davide Brunato <brunato@sissa.it>
+#
+"""
+This subpackage contains converter classes.
+"""
+from .default import ElementData, XMLSchemaConverter
+from .unordered import UnorderedConverter
+from .parker import ParkerConverter
+from .badgerfish import BadgerFishConverter
+from .abdera import AbderaConverter
+from .jsonml import JsonMLConverter
+from .columnar import ColumnarConverter
