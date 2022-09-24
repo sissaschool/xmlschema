3c30c7cb818fa915067d8e0fdde91f0e93acbece
Davide Brunato <brunato@sissa.it>
2022-09-24 08:13:42 +0200

    Align from_json() and to_etree() arguments

--- xmlschema/documents.py
+++ xmlschema/documents.py
@@ -15,7 +15,7 @@ from typing import Any, Dict, List, Optional, Type, Union, Tuple, \
 from elementpath.etree import ElementTree, etree_tostring
 
 from .exceptions import XMLSchemaTypeError, XMLSchemaValueError, XMLResourceError
-from .names import XSD_NAMESPACE, XSI_TYPE
+from .names import XSD_NAMESPACE, XSI_TYPE, XSD_SCHEMA
 from .aliases import ElementType, XMLSourceType, NamespacesType, LocationsType, \
     LazyType, SchemaSourceType, ConverterType, DecodeType, EncodeType, \
     JsonDecodeType
@@ -406,7 +406,7 @@ def to_etree(obj: Any,
 
     :param obj: the Python object that has to be encoded to XML data.
     :param schema: can be a schema instance or a file-like object or a file path or a URL \
-    of a resource or a string containing the schema.
+    of a resource or a string containing the schema. If not provided a dummy schema is used.
     :param cls: class to use for building the schema instance (for default uses \
     :class:`XMLSchema10`).
     :param path: is an optional XPath expression for selecting the element of the schema \
@@ -438,16 +438,24 @@ def to_etree(obj: Any,
             raise XMLSchemaTypeError("without schema a path is required "
                                      "for building a dummy schema")
 
-        tag = get_extended_qname(path, namespaces)
+        if namespaces is None:
+            tag = get_extended_qname(path, {'xsd': XSD_NAMESPACE, 'xs': XSD_NAMESPACE})
+        else:
+            tag = get_extended_qname(path, namespaces)
+
         if not tag.startswith('{') and ':' in tag:
             raise XMLSchemaTypeError("without schema the path must be "
                                      "mappable to a local or extended name")
 
-        _schema = get_dummy_schema(tag, cls)
+        if tag == XSD_SCHEMA:
+            _schema = cls.meta_schema
+        else:
+            _schema = get_dummy_schema(tag, cls)
+
     elif isinstance(schema, XMLSchemaBase):
         _schema = schema
     else:
-        _schema = cls(schema, **kwargs)
+        _schema = cls(schema)
 
     return _schema.encode(
         obj=obj,
@@ -463,8 +471,13 @@ def to_etree(obj: Any,
 
 def from_json(source: Union[str, bytes, IO[str]],
               schema: Optional[Union[XMLSchemaBase, SchemaSourceType]] = None,
+              cls: Optional[Type[XMLSchemaBase]] = None,
               path: Optional[str] = None,
+              validation: str = 'strict',
+              namespaces: Optional[NamespacesType] = None,
+              use_defaults: bool = True,
               converter: Optional[ConverterType] = None,
+              unordered: bool = False,
               json_options: Optional[Dict[str, Any]] = None,
               **kwargs: Any) -> EncodeType[ElementType]:
     """
@@ -473,11 +486,19 @@ def from_json(source: Union[str, bytes, IO[str]],
     :param source: can be a string or a :meth:`read()` supporting file-like object \
     containing the JSON document.
     :param schema: an :class:`XMLSchema10` or an :class:`XMLSchema11` instance.
+    :param cls: class to use for building the schema instance (for default uses \
+    :class:`XMLSchema10`).
     :param path: is an optional XPath expression for selecting the element of the schema \
     that matches the data that has to be encoded. For default the first global element of \
     the schema is used.
-    :param converter: an :class:`XMLSchemaConverter` subclass or instance to use \
-    for the encoding.
+    :param validation: the XSD validation mode. Can be 'strict', 'lax' or 'skip'.
+    :param namespaces: is an optional mapping from namespace prefix to URI.
+    :param use_defaults: whether to use default values for filling missing data.
+    :param converter: an :class:`XMLSchemaConverter` subclass or instance to use for \
+    the encoding.
+    :param unordered: a flag for explicitly activating unordered encoding mode for \
+    content model data. This mode uses content models for a reordered-by-model \
+    iteration of the child elements.
     :param json_options: a dictionary with options for the JSON deserializer.
     :param kwargs: other optional arguments of :meth:`XMLSchemaBase.iter_encode` and \
     options for converter.
@@ -495,7 +516,18 @@ def from_json(source: Union[str, bytes, IO[str]],
     else:
         obj = json.load(source, **json_options)
 
-    return to_etree(obj, schema=schema, path=path, converter=converter, **kwargs)
+    return to_etree(
+        obj=obj,
+        schema=schema,
+        cls=cls,
+        path=path,
+        validation=validation,
+        namespaces=namespaces,
+        use_defaults=use_defaults,
+        converter=converter,
+        unordered=unordered,
+        **kwargs
+    )
 
 
 class XmlDocument(XMLResource):
