764d8b195acf0f886fe83687521db779e84d2773
Konstanty Krzemień <konstanty.krzemien@mga.com.pl>
2023-07-07 08:01:44 +0200

    moved polish translation test cases

--- tests/test_translations.py
+++ tests/test_translations.py
@@ -109,8 +109,8 @@ class TestTranslations(unittest.TestCase):
             translation._translation = None
 
     def test_pl_validation_translation(self):
-        xml_path = "tests//test_cases//examples//polish_translation//tytul_wykonawczy_niekompletny.xml"
-        xsd_path = "tests//test_cases//examples//polish_translation//tw-1(5)8e.xsd"
+        xml_path = "tests//test_cases//translations//polish_translation//tytul_wykonawczy_niekompletny.xml"
+        xsd_path = "tests//test_cases//translations//polish_translation//tw-1(5)8e.xsd"
         expected_errors = [
             "Zawartość elementu 'com:Opisowy' nie jest kompletna. Oczekiwany znacznik 'com:Miejscowosc'.",
             "atrybut rodzaj='8': wartość musi być jedną z [1, 2]",
